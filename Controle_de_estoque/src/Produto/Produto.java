6edb40bf4d38fc24e5bafe2cc4e635a2cb085c40
Andressa Soares Rodrigues <rodrigues.andressasoares@gmail.com>
2021-08-10 14:31:58 -0300

    cria a primeira classe

---
+++ Controle_de_estoque/src/Produto/Produto.java
@@ -0,0 +1,48 @@
+public class Produto {
+    private String nome;
+    private double preco;
+    private String unidade;
+    private int quant;
+
+    public Produto() {}
+
+    public Produto(String nome, double preco, String un, int quant) {
+        this.nome = nome;
+        this.preco = preco;
+        this.unidade = un;
+        this.quant = quant;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuant() {
+        return quant;
+    }
+
+    public void setQuant(int quant) {
+        this.quant = quant;
+    }
+
+    public String getUnidade() {
+        return unidade;
+    }
+
+    public void setUnidade(String unidade) {
+        this.unidade = unidade;
+    }
+
+}
